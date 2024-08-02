2bb8e24a6fdbdecf3276a13f24c54bd711d9155b
Nigel Wright <88900013+jwnigel@users.noreply.github.com>
2024-08-02 19:06:47 -0400

    Initial Commit

---
+++ KBTimer/KBTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBTimerApp.swift
+//  KBTimer
+//
+//  Created by Nigel Wright on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct KBTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
